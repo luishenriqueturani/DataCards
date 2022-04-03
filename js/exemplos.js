d1fc12dedfd771e149dfdc78dc25b4b5592d3ffa
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2022-04-03 16:01:42 -0300

    Update exemplos.js

--- js/exemplos.js
+++ js/exemplos.js
@@ -1,10 +1,7 @@
 let pokemons
 
 document.addEventListener('DOMContentLoaded', async function () {
-
     pokemons = await buscarListaPokemons();
-
-    console.log(pokemons);
 
     let html = ``;
     let htmlSec = ``;
@@ -25,22 +22,20 @@ document.addEventListener('DOMContentLoaded', async function () {
                     <td class="imagem-pokemon">${imagem}</td>
                 </tr>
         `;
-
-        htmlSec += `
-        <tr>
-            <td class="id-pokemon">${pkmn.pokedex}</td>
-            <td class="nome-pokemon">${pkmn.name}</td>
-            <td class="tipo-pokemon">${pkmn.type}</td>
-            <td class="imagem-pokemon">${imagem}</td>
-        </tr>
+        htmlSec += `<tr>
+                        <td class="id-pokemon">${pkmn.pokedex}</td>
+                        <td class="nome-pokemon">${pkmn.name}</td>
+                        <td class="tipo-pokemon">${pkmn.type}</td>
+                        <td class="imagem-pokemon">${imagem}</td>
+                    </tr>
         `;
     }
 
     let tbDefault = $(`#tabela-modelo-default`);
     let tb2 = $(`#tabela-modelo-2`);
 
-    await tbDefault.find(`tbody`).html(html);
-    await tb2.find(`tbody`).html(htmlSec);
+    await tbDefault.find(`tbody`).html( $(html) );
+    await tb2.find(`tbody`).html( $(htmlSec) );
 
     await tbDefault.DataTable({
         "scrollX": true,
@@ -66,15 +61,13 @@ function buscarListaPokemons() {
     return new Promise(async function (resolve, reject) {
         try {
             var myHeaders = new Headers();
-
             let response = await fetch(`http://localhost:4001/get-pokemons`,{
                 method: 'GET',
-               headers: myHeaders,
-               mode: 'cors',
-               cache: 'default' 
+                headers: myHeaders,
+                mode: 'cors',
+                cache: 'default' 
             });
-            let retorno = response.json()
-            console.log(retorno)
+            let retorno = response.json();
             resolve(retorno);
         } catch (error) {
             reject(error);
